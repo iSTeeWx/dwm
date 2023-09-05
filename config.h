2467714e1018b6ecac7809f2d3a3695d7b916ef4
iSTeeWx_ <dev.isteewx@gmail.com>
2023-09-05 11:08:29 +0200

    added fibonacci layout with gaps

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-#include "fibonacci.c"
+#include "fibonacci_gaps.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
---
+++ fibonacci_gaps.c
@@ -0,0 +1,75 @@
+void
+dwindle(Monitor *m)
+{
+	unsigned int i, n;
+	int nx, ny, nw, nh;
+	int og = 5, ig = 5;
+	int nv, hrest = 0, wrest = 0, r = 1;
+	Client *c;
+
+    for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if (n == 0)
+		return;
+
+	nx = m->wx + og;
+	ny = m->wy + og;
+	nw = m->ww - 2 * og;
+	nh = m->wh - 2 * og;
+
+	for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
+		if (r) {
+			if ((i % 2 && (nh - ig) / 2 <= (bh + 2*c->bw))
+			   || (!(i % 2) && (nw - ig) / 2 <= (bh + 2*c->bw))) {
+				r = 0;
+			}
+			if (r && i < n - 1) {
+				if (i % 2) {
+					nv = (nh - ig) / 2;
+					hrest = nh - 2*nv - ig;
+					nh = nv;
+				} else {
+					nv = (nw - ig) / 2;
+					wrest = nw - 2*nv - ig;
+					nw = nv;
+				}
+			}
+
+			if ((i % 4) == 0) {
+                ny += nh + ig;
+                nh += hrest;
+			}
+			else if ((i % 4) == 1) {
+				nx += nw + ig;
+				nw += wrest;
+			}
+			else if ((i % 4) == 2) {
+				ny += nh + ig;
+				nh += hrest;
+				if (i < n - 1)
+					nw += wrest;
+			}
+			else if ((i % 4) == 3) {
+                nx += nw + ig;
+                nw -= wrest;
+			}
+			if (i == 0)	{
+				if (n != 1) {
+					nw = (m->ww - ig - 2*og) - (m->ww - ig - 2*og) * (1 - m->mfact);
+					wrest = 0;
+				}
+				ny = m->wy + og;
+			}
+			else if (i == 1)
+				nw = m->ww - nw - ig - 2*og;
+			i++;
+		}
+
+		resize(c, nx, ny, nw - (2*c->bw), nh - (2*c->bw), False);
+	}
+}
+
+void
+dwindle(Monitor *m)
+{
+	fibonacci(m);
+}
