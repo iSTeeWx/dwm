79161b2f444ecc003cf1dbe49116256d6f5104b2
iSTeeWx <dev.isteewx@gmail.com>
2024-07-27 00:01:46 +0200

    removed tag 6 7 8 9

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -96,10 +96,10 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_quotedbl,                           2)
 	TAGKEYS(                        XK_apostrophe,                         3)
 	TAGKEYS(                        XK_parenleft,                          4)
-	TAGKEYS(                        XK_minus,                              5)
-	TAGKEYS(                        XK_egrave,                             6)
-	TAGKEYS(                        XK_underscore,                         7)
-	TAGKEYS(                        XK_ccedilla,                           8)
+	// TAGKEYS(                        XK_minus,                              5)
+	// TAGKEYS(                        XK_egrave,                             6)
+	// TAGKEYS(                        XK_underscore,                         7)
+	// TAGKEYS(                        XK_ccedilla,                           8)
 };
 
 /* button definitions */
