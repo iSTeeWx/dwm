c36df1df0f497ea62d085825fead68721362c899
iSTeeWx <dev.isteewx@gmail.com>
2024-09-08 04:02:14 +0200

    change "@DEFAULT_SINK@" to shorter "0"

--- scripts/volume-helper.sh
+++ scripts/volume-helper.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 if [[ $1 == "up" ]]; then
-	pactl set-sink-mute @DEFAULT_SINK@ false
-	pactl set-sink-volume @DEFAULT_SINK@ +5%
+	pactl set-sink-mute 0 false
+	pactl set-sink-volume 0 +5%
 
-	if [[ $(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1) -ge 100 ]]; then
-		pactl set-sink-volume @DEFAULT_SINK@ 100%
+	if [[ $(pactl get-sink-volume 0 | grep -Po '\d+(?=%)' | head -n 1) -ge 100 ]]; then
+		pactl set-sink-volume 0 100%
 	fi
 
 elif [[ $1 == "down" ]]; then
-	pactl set-sink-volume @DEFAULT_SINK@ -5%
+	pactl set-sink-volume 0 -5%
 
-	if [[ $(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1) -le 0 ]]; then
-		pactl set-sink-mute @DEFAULT_SINK@ true
+	if [[ $(pactl get-sink-volume 0 | grep -Po '\d+(?=%)' | head -n 1) -le 0 ]]; then
+		pactl set-sink-mute 0 true
 	else
-		pactl set-sink-mute @DEFAULT_SINK@ false
+		pactl set-sink-mute 0 false
 	fi
 elif [[ $1 == "mute" ]]; then
-	pactl set-sink-mute @DEFAULT_SINK@ toggle
+	pactl set-sink-mute 0 toggle
 fi
 
 sh /home/vlad/.local/src/dwmstatus/dwmstatus-restart
-echo $(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1)
-echo $(pactl get-sink-mute @DEFAULT_SINK@)
+echo $(pactl get-sink-volume 0 | grep -Po '\d+(?=%)' | head -n 1)
+echo $(pactl get-sink-mute 0)
