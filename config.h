475b5b50578f58c4fb44b6dea4f21a7fb4ac499c
isteewx <dev.isteewx@gmail.com>
2025-01-07 00:52:44 +0100

    change font

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
@@ -6,8 +7,8 @@ static const unsigned int gappx     = 4;        /* gap between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono NF:size=10:style=Bold" };
-static const char dmenufont[]       = "JetBrainsMono NF:size=10:style=Bold";
+static const char *fonts[]          = { "SauceCodePro NFM:size=14:style=Bold" };
+static const char dmenufont[]       = "SauceCodePro NFM:size=14:style=Bold";
 static const char col_gray1[]       = "#282A36";  /* Bar color */
 static const char col_gray2[]       = "#282A36";  /* Border non selected */
 static const char col_gray3[]       = "#F8F8F2";  /* Bar text color */
@@ -16,7 +17,7 @@ static const char col_accent[]      = "#7287fd";  /* Tag selected bg, or border 
 static const char *colors[][3]      = {
 	/*               fg         bg          border   */
 	[SchemeNorm] = { col_gray3, col_gray1,  col_gray2 },
-	[SchemeSel]  = { col_gray4, col_accent, col_accent  },
+	[SchemeSel]  = { col_gray4, col_accent, col_accent },
 };
 
 /* tagging */
@@ -65,8 +66,6 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static const char *volumeupcmd[]   = { "sh", "/home/vlad/.local/src/dwm/scripts/volume-helper.sh", "up", NULL };
 static const char *volumedowncmd[] = { "sh", "/home/vlad/.local/src/dwm/scripts/volume-helper.sh", "down", NULL };
 static const char *volumemutecmd[] = { "sh", "/home/vlad/.local/src/dwm/scripts/volume-helper.sh", "mute", NULL };
-
-#include <X11/XF86keysym.h>
 
 static const Key keys[] = {
 	/* modifier                     key                    function        argument */
