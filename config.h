7aaa027228a7123d122296f6e00c9046506a6add
isteewx <dev.isteewx@gmail.com>
2025-01-07 01:12:01 +0100

    open browser on ^b

--- config.h
+++ config.h
@@ -62,6 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_accent, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browsercmd[]  = { "chromium", NULL };
 
 static const char *volumeupcmd[]   = { "sh", "/home/vlad/.local/src/dwm/scripts/volume-helper.sh", "up", NULL };
 static const char *volumedowncmd[] = { "sh", "/home/vlad/.local/src/dwm/scripts/volume-helper.sh", "down", NULL };
@@ -74,7 +75,7 @@ static const Key keys[] = {
 	{ 0,                            XF86XK_AudioLowerVolume, spawn,          {.v = volumedowncmd } },
 	{ MODKEY,                       XK_space,                spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,               spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,                    togglebar,      {0} },
+	{ MODKEY,                       XK_b,                    spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_j,                    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                    focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,                    incnmaster,     {.i = +1 } },
