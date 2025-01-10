1515382fb6a8ff62c9a2d54553ee5603f314d49b
isteewx <dev.isteewx@gmail.com>
2025-01-10 17:49:25 +0100

    hard code max battery capacity on my laptop it changed from 3180mAh to 3920mAh, so my laptop was constantly at 80% when not plugged in

--- scripts/battery-notification.sh
+++ scripts/battery-notification.sh
@@ -1,4 +1,7 @@
-max_charge=$(cat /sys/class/power_supply/BAT0/charge_full_design)
+# max_charge=$(cat /sys/class/power_supply/BAT0/charge_full_design)
+
+# hard code because it changed when plugged in ...
+max_charge=3180000
 current_charge=$(cat /sys/class/power_supply/BAT0/charge_now)
 charge=$(echo "scale=2; $current_charge/$max_charge*100" | bc)
 charge=$(echo $charge/1 | bc)
