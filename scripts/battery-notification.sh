8ab206042d8c82446ca66171247bae718f826b4b
isteewx <dev.isteewx@gmail.com>
2025-01-10 17:52:46 +0100

    Revert "hard code max battery capacity"
    This reverts commit 1515382fb6a8ff62c9a2d54553ee5603f314d49b.

--- scripts/battery-notification.sh
+++ scripts/battery-notification.sh
@@ -1,7 +1,4 @@
-# max_charge=$(cat /sys/class/power_supply/BAT0/charge_full_design)
-
-# hard code because it changed when plugged in ...
-max_charge=3180000
+max_charge=$(cat /sys/class/power_supply/BAT0/charge_full_design)
 current_charge=$(cat /sys/class/power_supply/BAT0/charge_now)
 charge=$(echo "scale=2; $current_charge/$max_charge*100" | bc)
 charge=$(echo $charge/1 | bc)
